e785411f15e0e5cbf5cf034fcaf3c311307bf850
xoxocrystyle <crystal.shir@gmail.com>
2019-07-15 10:56:07 -0700

    added form validation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Book Class: Represents a book 
 class Book {
-    constructor(title, author, isbn){
+    constructor(title, author, isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
@@ -9,8 +9,7 @@ class Book {
 //UI Class: Handle UI Tasks 
 class UI {
     static displayBooks() {
-        const StoredBooks = [
-            {
+        const StoredBooks = [{
                 title: 'Book One',
                 author: 'John Doe',
                 isbn: '12345'
@@ -25,7 +24,7 @@ class UI {
 
         books.forEach((book) => UI.addBookToList(book));
     }
-    static addBookToList(book){
+    static addBookToList(book) {
         const list = document.querySelector('#book-list');
 
         const row = document.createElement('tr');
@@ -40,16 +39,30 @@ class UI {
         list.appendChild(row);
     }
 
-    static deleteBook(el){
-        if(el.classList.contains('delete')){
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
             el.parentElement.parentElement.remove();
         }
     }
 
-    static clearFields(){
-        const title = document.querySelector('#title').value = ''; 
+    static clearFields() {
+        const title = document.querySelector('#title').value = '';
         const author = document.querySelector('#author').value = '';
         const isbn = document.querySelector('#isbn').value = '';
+    }
+
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 
+        2000);
     }
 }
 //Store Class: Handle Storage
@@ -68,21 +81,26 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const isbn = document.querySelector('#isbn').value;
 
     //validate
-    if(title === '' || author === '' || isbn === ''){
-        alert('Please fill in all fields');
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields.', 'danger');
+    } else {
+        //instatiate book
+        const book = new Book(title, author, isbn);
+
+        //Add Book to UI
+        UI.addBookToList(book);
+
+        //Show success message
+        UI.showAlert('Book Added', 'success');
+
+        //clear fields 
+        UI.clearFields();
     }
-
-    //instatiate book
-    const book = new Book(title, author, isbn);
-
-    //Add Book to UI
-    UI.addBookToList(book);
-
-    //clear fields 
-    UI.clearFields();
 })
 
 //Event: remove a book 
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target);
+
+    UI.showAlert('Book Removed.', 'success');
 })
