a237d0293736576822b174fe8c8d73b85729f629
xoxocrystyle <crystal.shir@gmail.com>
2019-07-15 12:12:30 -0700

    created class  to handle local storage

--- app.js
+++ app.js
@@ -9,21 +9,11 @@ class Book {
 //UI Class: Handle UI Tasks 
 class UI {
     static displayBooks() {
-        const StoredBooks = [{
-                title: 'Book One',
-                author: 'John Doe',
-                isbn: '12345'
-            },
-            {
-                title: 'Book Two',
-                author: 'Jane Doe',
-                isbn: '45678'
-            }
-        ];
-        const books = StoredBooks;
+        const books = Store.getBooks();
 
         books.forEach((book) => UI.addBookToList(book));
     }
+
     static addBookToList(book) {
         const list = document.querySelector('#book-list');
 
@@ -66,7 +56,36 @@ class UI {
     }
 }
 //Store Class: Handle Storage
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
 
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if(book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
 //Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
@@ -90,6 +109,9 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
         //Add Book to UI
         UI.addBookToList(book);
 
+        //add book to store
+        Store.addBook(book);
+
         //Show success message
         UI.showAlert('Book Added', 'success');
 
@@ -101,6 +123,9 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 //Event: remove a book 
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target);
+
+    //Remove book from storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     UI.showAlert('Book Removed.', 'success');
 })
